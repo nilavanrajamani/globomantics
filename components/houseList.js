8ead22eacee6e5055037df946a000e55d47ddbd6
Nilavan Rajamani <nilavanrajamani@gmail.com>
2025-02-25 22:38:48 +0530

    hooks added

--- components/houseList.js
+++ components/houseList.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import HouseRow from "./houseRow";
 
 const houseArray = [
@@ -16,28 +17,40 @@ const houseArray = [
 ];
 
 const HouseList = () => {
-    return(
-    <>
-        <div className="row mb-2">
-            <h5 className="themeFontColor text-center">
-                Houses currently on the market
-            </h5>
-        </div>
-        <table className="table table-hover">
-            <thead>
-                <tr>
-                    <th>Address</th>
-                    <th>Country</th>
-                    <th>Asking Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                {houseArray.map(x => (
-                    <HouseRow house={x} key={x.id}></HouseRow>
-                ))}                
-            </tbody>
-        </table>
-    </>
+    const [houses, setHouses] = useState(houseArray);
+
+    const addHouse = () => {
+        setHouses([...houses, {
+            id: 3,
+            address: "32 Valley way, New York",
+            country: "USA",
+            price: 100000,
+        },]);
+    };
+
+    return (
+        <>
+            <div className="row mb-2">
+                <h5 className="themeFontColor text-center">
+                    Houses currently on the market
+                </h5>
+            </div>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Address</th>
+                        <th>Country</th>
+                        <th>Asking Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {houses.map(x => (
+                        <HouseRow house={x} key={x.id}></HouseRow>
+                    ))}
+                </tbody>
+            </table>
+            <button className="btn btn-primary" onClick={addHouse}>Add</button>
+        </>
     );
 };
 
